e691ae934471d4187402939e43c7b49e6b3d039d
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-03-01 15:19:59 +0530

    Add files via upload

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1440,7 +1440,7 @@ logo () {
 	printf "\n$lal [ 25 ] $pila Twrp Tool"
 	printf "	$hara [ 26 ] $ajib Happy New Year\n "
 	printf "\n$lal [ 27 ] $pila C-Fish\n"
-	printf "	$hara [ 28 ] $ajib Update-Tool"
+	printf "		$hara [ 28 ] $ajib Update-Tool"
 	printf "\n$lal [ 29 ] $pila Exit\n\n\n"
 	printf "\033[92m Checking updates....\n"
 	version
